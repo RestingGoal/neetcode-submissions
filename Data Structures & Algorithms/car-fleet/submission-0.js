dcc2745553448e9258c6cc3df0b113418a766091
Resting Goal <64196621+RestingGoal@users.noreply.github.com>
2026-08-12 17:37:24 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        let cars = [];
+
+        for (let i = 0; i < position.length; i++) {
+            cars.push([
+                position[i],
+                (target - position[i]) / speed[i]
+            ]);
+        }
+
+        // Closest to target first
+        cars.sort((a, b) => b[0] - a[0]);
+
+        let stack = [];
+
+        for (let [pos, time] of cars) {
+            if (stack.length === 0 || time > stack[stack.length - 1]) {
+                stack.push(time);
+            }
+        }
+
+        return stack.length;
+    }
+    }
+
