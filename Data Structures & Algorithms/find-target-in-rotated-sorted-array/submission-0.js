ef6dcf73a8f6d01c0d66314c620aa96ecc6b92cb
Resting Goal <64196621+RestingGoal@users.noreply.github.com>
2026-08-14 01:04:32 +0530

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l < r) {
+            let m = Math.floor((l + r) / 2);
+
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+
+        const pivot = l;
+        //No of rotations = index of the smallest number, because order is ascending
+        const result = this.binarySearch(nums, target, 0, pivot - 1);
+
+        if (result !== -1) {
+            return result;
+        }
+
+        return this.binarySearch(nums, target, pivot, nums.length - 1);
+    }
+    binarySearch(nums, target, left, right) {
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+    }
+
