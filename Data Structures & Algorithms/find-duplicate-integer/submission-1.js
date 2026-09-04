d091a9ef9bb9a2a02ece360096bcbc710337a7bb
Resting Goal <64196621+RestingGoal@users.noreply.github.com>
2026-09-05 00:37:56 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        let Set1 = new Set();
+        for(let i of nums){
+            if(!Set1.has(i)) Set1.add(i);
+            else return i;
+        }
+        //O(n) space
+    }
+}
